4860c92593b7383f462b278c52295e2c7bdec81e
Shehab Elhariry <cheha6@gmail.com>
2020-11-17 16:15:22 +0200

    edited the style of the steps indicator

--- src/components/Steps/StepsIndicator/StepsIndicator.js
+++ src/components/Steps/StepsIndicator/StepsIndicator.js
@@ -3,15 +3,55 @@
 import { jsx } from "theme-ui";
 
 const StepsIndicator = ({ allSteps, currentStep }) => {
+  const stepsLabels = [
+    "Template Selection",
+    "Contact Information",
+    "Social Links",
+    "Work Experience",
+    "Education",
+    "Skills",
+    "Languages",
+  ];
   return (
-    <div sx={{ display: "flex" }}>
-      {new Array(allSteps).fill(1).map((step, index) => (
-        <div
+    <div>
+      <p
+        sx={{
+          ml: "10px",
+          color: "#195a75",
+          display: "flex",
+          alignItem: "center",
+          mb: "5px",
+        }}
+      >
+        <span
           sx={{
-            variant: index === currentStep ? "stepCompleted" : "stepPending",
+            // height: "25px",
+            // width: "25px",
+            // borderRadius: "100%",
+            border: "1px solid",
+            borderColor: "primary",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mr: "10px",
+            p: "5px",
           }}
-        ></div>
-      ))}
+        >
+          {currentStep + 1} / {allSteps}
+        </span>
+        <span sx={{ display: "flex", lineHeight: "35px" }}>
+          {stepsLabels[currentStep]}
+        </span>
+      </p>
+      <div sx={{ display: "flex" }}>
+        {new Array(allSteps).fill(1).map((step, index) => (
+          <div
+            sx={{
+              variant: index === currentStep ? "stepCompleted" : "stepPending",
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
